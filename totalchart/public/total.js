f2f85b386cb65cb6f3483ea4c0ca42de1a805d28
LEEJIWOOE <sk9311@naver.com>
2024-02-20 16:14:31 +0900

    totalchart를 html - oracledb와 연동해서 구현완료 // readme 파일을 꼭 읽어주시고 맞춰서 경로설정해주세요.

---
+++ totalchart/public/total.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const oracledb = require('oracledb');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+oracledb.autoCommit = true;
+
+
+app.use(cors());
+app.use(express.static('public'));
+
+
+async  function getTotalData() {
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: 'open_source',
+            password: '1111',
+            connectString:'localhost:1521/xe'
+        });
+        const result = await connection.execute(
+            `select total from project`,
+             );
+            return result.rows;
+    }finally {
+        if(connection){
+            await connection.close();
+        }
+    }
+}
+
+oracledb.initOracleClient({ libDir: '../CHART/instantclient_21_13' });
+app.get('/api/total', async (req, res) => {
+    try {
+        const data = await getTotalData();
+
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/api/total`);
+});
