8c225a4eb447b5a31f012461bfa17528ef87572c
hojinbae <153696184+hojinbae@users.noreply.github.com>
2024-03-04 16:04:15 +0900

    HJBae
    게시판마무리1

--- Gangnam/server.js
+++ Gangnam/server.js
@@ -22,17 +22,17 @@ app.set('view engine', 'ejs');
 oracledb.initOracleClient({ libDir: 'C:\\instantclient_21_13' });
 oracledb.autoCommit = true;
 
-app.use('/boardMain', require('./routes/boardMain2')); //게시판 메인 페이지 렌더링
-app.use('/addComment', require('./routes/addComment2')); //댓글 페이지 렌더링
-app.use('/login', require('./routes/login2')); // 로그인 처리
-app.use('/loginFail', require('./routes/loginFail2')); // 로그인 실패 처리
-app.use('/logout', require('./routes/logout2')); // 로그아웃 처리
-app.use('/create', require('./routes/create2')); // 글 작성 페이지 렌더링
-app.use('/detailPost', require('./routes/detailPost2')); // 상세 페이지 렌더링
-app.use('/editPost', require('./routes/editPost2')); // 수정 페이지 렌더링
-app.use('/deletePost', require('./routes/deletePost2')); // 삭제 처리
-// app.use('/deleteComment', require('./routes/deleteComment2')); // 댓글 삭제 처리
-// app.use('/editComLJW', require('./routes/editComLJW2')); // 댓글 수정 엔드포인트 추가
+app.use('/boardMain', require('./routes/boardMain')); //게시판 메인 페이지 렌더링
+app.use('/addComment', require('./routes/addComment')); //댓글 페이지 렌더링
+app.use('/login', require('./routes/login')); // 로그인 처리
+app.use('/loginFail', require('./routes/loginFail')); // 로그인 실패 처리
+app.use('/logout', require('./routes/logout')); // 로그아웃 처리
+app.use('/create', require('./routes/create')); // 글 작성 페이지 렌더링
+app.use('/detailPost', require('./routes/detailPost')); // 상세 페이지 렌더링
+app.use('/editPost', require('./routes/editPost')); // 수정 페이지 렌더링
+app.use('/deletePost', require('./routes/deletePost')); // 삭제 처리
+app.use('/deleteComment', require('./routes/deleteComment')); // 댓글 삭제 처리
+app.use('/editComment', require('./routes/editComment')); // 댓글 수정 엔드포인트 추가
 
 // 게시판 서버 시작
 app.listen(port, () => {
